226294ac83722217549a2d0a6395d06a9bcae410
Mathias Langlet <mathias.langlet@gmail.com>
2022-12-05 00:50:58 +0100

    Apply gofmt

--- go/day1/day1.go
+++ go/day1/day1.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-       "bufio"
-       "fmt"
-       "os"
-       "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func check(e error) {
-     if e != nil {
-     	panic(e)
-     }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func main(){
-     f, err := os.Open("../../input/day1")
-     defer f.Close()
-     check(err)
+func main() {
+	f, err := os.Open("../../input/day1")
+	defer f.Close()
+	check(err)
 
-     max, current := 0, 0
-     
-     fileScanner := bufio.NewScanner(f)
-     for fileScanner.Scan() {
-      	 text := fileScanner.Text()
-     	 if text == "" {
-	 	    if current > max {
-		 	    max = current
-		    }
-		    current = 0
-         } else {
-	        v, _ := strconv.Atoi(text)
-	        current += v
-	     }
-     }
-     
-     fmt.Printf("The most carried calories is %v!\n", max)
+	max, current := 0, 0
+
+	fileScanner := bufio.NewScanner(f)
+	for fileScanner.Scan() {
+		text := fileScanner.Text()
+		if text == "" {
+			if current > max {
+				max = current
+			}
+			current = 0
+		} else {
+			v, _ := strconv.Atoi(text)
+			current += v
+		}
+	}
+
+	fmt.Printf("The most carried calories is %v!\n", max)
 }
